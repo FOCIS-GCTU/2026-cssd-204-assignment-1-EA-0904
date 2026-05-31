c99dffcaaba3bf3b6fc555e23bbd23c56eafb0a5
EA-0904 <2425400904@live.gctu.edu.gh>
2026-05-31 22:15:20 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: A printout of the initials "EAA" in large size
+#Assignment Number: 2
+#
+#Name: Ernest Asamoah Antwi
+#Student Number: 2425400904
+#Email: 2425400904@live.gctu.edu.gh
+#Gender: male
+#
 
 def main():
-  pass #code goes here
 
+    print()
 
+print("...EAA")
+print()
+print("...EEEEEEEEEEE....AAAAA..........AAAAA.....")
+print("...EEEEEEEEEEE...AAAAAAA........AAAAAAA....")
+print("...EE...........AA.....AA......AA.....AA...")
+print("...EE..........AA.......AA....AA.......AA..")
+print("...EEEEEEEEE...AAAAAAAAAAA....AAAAAAAAAAA..")
+print("...EEEEEEEEE...AAAAAAAAAAA....AAAAAAAAAAA..")
+print("...EE..........AA.......AA....AA.......AA..")
+print("...EE..........AA.......AA....AA.......AA..")
+print("...EEEEEEEEEEE.AA.......AA..**AA.......AA**")
+print("...EEEEEEEEEEE.AA.......AA..**AA.......AA**")
 
+print()
+ 
 main()
+
